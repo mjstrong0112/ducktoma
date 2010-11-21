74d257bf77e288824073c0bff420c88d31ab2f6c
Paul Strong <paul@thestrongfamily.org>
2010-11-21 17:51:42 -0600

    initial rails commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ducktoma::Application.config.secret_token = 'e8b99a383c9bcb4174535ad89f685b59dab8f905d9c14ab03e7a923b9d6ba2002ea13bd8a9dc22ead664c3ebea210d61ae2c0ac54d79e7dc964ef440be0e8d25'
