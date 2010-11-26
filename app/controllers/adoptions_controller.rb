7049f13210b848236f9a8639241fa1be03ed5f3d
James Strong <laspluviosillas@gmail.com>
2010-11-25 18:38:01 -0600

    Fix to stub show page

--- app/controllers/adoptions_controller.rb
+++ app/controllers/adoptions_controller.rb
@@ -6,10 +6,10 @@ class AdoptionsController < ApplicationController
   before_filter :authenticate_user!, :only => :index
   
   def show
-    #@adoption = Adoption.find(params[:id])
+    @adoption = Adoption.find(params[:id])
     
     #Fake adoption
-    @adoption = Adoption.new
-    @adoption.raffle_number = "M25919385919401"
+    #@adoption = Adoption.new
+    #@adoption.raffle_number = "M25919385919401"
   end
 end
