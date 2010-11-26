d6f146ac097f614467a1992ce9b506153622d6bf
James Strong <laspluviosillas@gmail.com>
2010-11-25 18:36:04 -0600

    Stub show page

--- app/controllers/adoptions_controller.rb
+++ app/controllers/adoptions_controller.rb
@@ -4,4 +4,12 @@ class AdoptionsController < ApplicationController
   belongs_to :user, :optional => :true
 
   before_filter :authenticate_user!, :only => :index
+  
+  def show
+    #@adoption = Adoption.find(params[:id])
+    
+    #Fake adoption
+    @adoption = Adoption.new
+    @adoption.raffle_number = "M25919385919401"
+  end
 end
---
+++ spec/views/adoptions/show.html.haml_spec.rb
@@ -0,0 +1,22 @@
+require 'views/views_helper'
+
+describe "adoptions/show.html.haml" do
+  before do
+    @adoption = assign(:adoption, Fabricate(:adoption))
+  end
+
+  it "should contain the raffle number" do
+    render
+    rendered.should have_selector('#raffle-number', :content => @adoption.raffle_number)
+  end
+
+  it "should contain a print button" do
+    render
+    rendered.should have_selector('#print', :class => "blue-button")
+  end
+
+  it "should contain total" do
+    render
+    rendered.should have_selector('div', :content => @adoption.fee.to_s)
+  end
+end
