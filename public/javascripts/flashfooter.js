320e1ef4446b415216d66a2434ef1f57a47fe60c
James Strong <laspluviosillas@gmail.com>
2010-11-29 22:21:10 -0600

    Add real logo, new ducks to swf, and sponsors

--- public/javascripts/flashfooter.js
+++ public/javascripts/flashfooter.js
@@ -2,6 +2,6 @@ $(document).ready(function() {
 	var params = { bgcolor: "#7cbbd9" };
 	var attributes = {};
 	var flashvars = {};
-	swfobject.embedSWF("/duck.swf", "insert", "100%", "180", "9.0.0", "expressInstall.swf", flashvars, params, attributes);				   
+	swfobject.embedSWF("/duck.swf", "insert", "100%", "140", "9.0.0", "expressInstall.swf", flashvars, params, attributes);
 });
 
