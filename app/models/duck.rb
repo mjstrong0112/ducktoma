9a4f8c47d1cb56d7c99bdebf757d0414fc4edb39
Paul Strong <paul@thestrongfamily.org>
2010-11-30 00:50:44 -0600

    Make duck number immutable

--- app/models/duck.rb
+++ app/models/duck.rb
@@ -9,12 +9,12 @@ class Duck
 
   before_create :generate_number
 
-  #def number= count
-  #  @number ||= count
-  #end
+  def number= number
+    read_attribute(:number) || write_attribute(:number, number)
+  end
 
   def generate_number
     last_duck_number = Duck.max(:number)
-    self.number = last_duck_number ? (last_duck_number + 1) : 1
+    @number = last_duck_number ? (last_duck_number + 1) : 1
   end
 end
