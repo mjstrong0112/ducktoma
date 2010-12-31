836ac96f093655986f442d92073c7da9120f58c4
Paul Strong <paul@thestrongfamily.org>
2010-12-30 23:24:02 -0600

    Clean up dashboard controller

--- app/controllers/sales/dashboard_controller.rb
+++ app/controllers/sales/dashboard_controller.rb
@@ -1,11 +1,5 @@
 class Sales::DashboardController < Sales::BaseController
-  #load_and_authorize_resource
   def index
 
   end
-  #belongs_to :user, :optional => :true
-  #before_filter :authenticate_user!, :only => :index
-  #def index
-    #@adoptions = Adoption.paginate(:page => params[:page] ||= 1)
-  #end
 end
