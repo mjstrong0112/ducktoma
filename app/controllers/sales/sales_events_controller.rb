06eccde45dbb2c7f4c26aa85c9a3373cbced08fa
James Strong <laspluviosillas@gmail.com>
2011-02-04 11:02:50 -0600

    Fix redirect on new sales_event

--- app/controllers/sales/sales_events_controller.rb
+++ app/controllers/sales/sales_events_controller.rb
@@ -2,7 +2,5 @@ class Sales::SalesEventsController < ApplicationController
   inherit_resources
   actions :index, :new, :create, :edit, :update, :destroy
   load_and_authorize_resource
-  def index
-    index!
-  end
+  create! { new_sales_sales_event_adoption_path(@sales_event) }
 end
