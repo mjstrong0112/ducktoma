a4bd58c4ab1a2c369c2ae6e4a4d31de600fcedb8
James Strong <jstrong@james-mac.gateway.2wire.net>
2011-03-11 15:02:42 -0600

    501c number and removed print button from confirm page

--- public/javascripts/flashfooter.js
+++ public/javascripts/flashfooter.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
 	var flashvars = {};
     if (screen.height>768) {
 	    swfobject.embedSWF("/duck.swf", "insert", "100%", "140", "9.0.0", "expressInstall.swf", flashvars, params, attributes);
-        $("#push").height(235);
+        $("#push").height(253);
     }
 });
 
