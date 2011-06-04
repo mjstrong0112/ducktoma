7385e136a28dfba55fd494acdd2e799b2b65bc42
James Strong <jstrong@james-mac.gateway.2wire.net>
2011-06-03 20:55:02 -0500

    view refactoring.

--- app/controllers/admin/adoptions_controller.rb
+++ app/controllers/admin/adoptions_controller.rb
@@ -1,9 +1,22 @@
 class Admin::AdoptionsController < Admin::BaseController
   inherit_resources
+  actions :index
+  
   load_and_authorize_resource
   belongs_to :user, :optional => :true
+
   #before_filter :authenticate_user!, :only => :index
-  def index
-    @adoptions = Adoption.paginate(:page => params[:page] ||= 1)
+  def index    
+    @adoptions_count = Adoption.valid.count
+
+    @total_ducks = 0
+    @total_donations = 0
+
+    Adoption.paid.each { |adoption|
+      @total_ducks += adoption.duck_count
+      @total_donations += adoption.fee
+    }
+
+    @adoptions = Adoption.valid.paginate(:page => params[:page] ||= 1, :per_page => 20)
   end
 end
--- app/helpers/adoptions_helper.rb
+++ app/helpers/adoptions_helper.rb
@@ -12,4 +12,15 @@ module AdoptionsHelper
   def pricing_json
     Pricing.desc(:quantity).map {|p| {:quantity => p.quantity, :price => p.price}}.to_json
   end
+  def contact_info_item(contact_info, field)
+    unless contact_info[field].blank?
+      raw (
+        content_tag(:p, field.to_s.titleize + ":") +
+        content_tag(:p, contact_info[field]) +
+        content_tag(:br)
+      )
+    else
+      nil
+    end
+  end
 end
