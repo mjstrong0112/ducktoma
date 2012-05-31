10678df5cba52baa8c7332ef5b2bbd54b9c9383d
James Strong <laspluviosillas@gmail.com>
2012-05-31 13:49:04 -0500

    refactoring

--- app/controllers/sales/sales_events_controller.rb
+++ app/controllers/sales/sales_events_controller.rb
@@ -7,26 +7,26 @@ class Sales::SalesEventsController < Sales::BaseController
   end
   def create
     @sales_event = SalesEvent.where(params[:sales_event]).first
-    unless params[:adoption_numbers]
-      unless @sales_event
-        create! { new_sales_sales_event_adoption_path(@sales_event) }
-      else
-        redirect_to new_sales_sales_event_adoption_path(@sales_event)
-      end
-    else
-      # If this is a reassignation
-      # Create sales event if necessary
-      if @sales_event.nil?
-        @sales_event = SalesEvent.create!(params[:sales_event])
-      end
-
-      adoption_numbers = params[:adoption_numbers].split(',')
-      adoptions = Adoption.where(:adoption_number.in => adoption_numbers).to_a
-      adoptions.each do |adoption|
-         adoption.update_attributes(:sales_event_id => @sales_event.id)
-      end
-
-      redirect_to admin_sales_events_url, :notice => "Reassign completed successfuly!"
+    # Create sales event if necessary
+    if @sales_event.nil?
+      @sales_event = SalesEvent.create!(params[:sales_event])
     end
+
+    # if this is a reassign operation, fork off to the
+    # appropriate function
+    return reassign if params[:adoption_numbers]
+
+    redirect_to new_sales_sales_event_adoption_path(@sales_event)
+  end
+
+  private
+  def reassign
+    adoption_numbers = params[:adoption_numbers].split(',')
+    adoptions = Adoption.where(:adoption_number.in => adoption_numbers).to_a
+    adoptions.each do |adoption|
+       adoption.update_attributes(:sales_event_id => @sales_event.id)
+    end
+
+    redirect_to admin_sales_events_url, :notice => "Reassign completed successfully!"
   end
 end
