dbea78762b6652d985a034e8b24ff985dd180931
James Strong <laspluviosillas@gmail.com>
2013-05-10 23:57:49 -0500

    redirect club leaders to profile

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,15 @@ class ApplicationController < ActionController::Base
 
   alias_method :current_devise_user, :current_user
 
+
+  def after_sign_in_path_for(resource)    
+    if resource.class == ClubMember
+      profile_path
+    else
+      root_path
+    end
+  end
+
   rescue_from CanCan::AccessDenied do |exception|
     flash[:error] = exception.message
     redirect_to root_url
