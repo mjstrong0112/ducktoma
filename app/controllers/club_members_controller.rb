58c76d3433dce78c3041fb08c0be09c711a59b7f
James Strong <laspluviosillas@gmail.com>
2013-05-11 02:20:35 -0500

    Try merging in oauth token, but not oauth_expires_at

--- app/controllers/club_members_controller.rb
+++ app/controllers/club_members_controller.rb
@@ -70,7 +70,7 @@ class ClubMembersController < ApplicationController
         u.uid = @fb_member.uid
         u.email = @fb_member.email if not @fb_member.email.blank?
         u.name = @fb_member.name
-        #u.oauth_token = @fb_member.oauth_token
+        u.oauth_token = @fb_member.oauth_token
         #u.oauth_expires_at = @fb_member.oauth_expires_at
         u.cache_photo
       end
