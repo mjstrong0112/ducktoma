05cef297735eafcb257a02e1416b8ee3789a15b2
James Strong <laspluviosillas@gmail.com>
2013-05-12 23:50:38 -0500

    Changing tmp dir for heroku production.

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,4 +1,7 @@
 CarrierWave.configure do |config|
+  config.root = Rails.root.join('tmp')
+  config.cache_dir = 'carrierwave'
+
   config.fog_credentials = {
     :provider               => 'AWS',
     :aws_access_key_id      => 'AKIAJ6L6AVJBLUDZNQXQ',
